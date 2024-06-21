39416f54fb128d35900ff08eaf3b89ce3692e457
Sophia Orlando <66695379+sophiaorlando@users.noreply.github.com>
2024-06-21 18:44:10 +0000

    added new text file and js file

---
+++ sophiaO/sophiaCode.js
@@ -0,0 +1,7 @@
+// create a javascript funciton that adds two numbers together and returns the sum of the two numbers
+
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(addTwoNumbers(2, 3)); // 5
